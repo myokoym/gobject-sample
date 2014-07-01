9a7a21009079d8eb6c34e8e97c959dd84b8fb221
Masafumi Yokoyama <myokoym@gmail.com>
2014-07-01 23:18:56 +0900

    Create a project using GObject

---
+++ autogen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -u
+set -e
+
+mkdir -p m4
+autoreconf --install
---
+++ sample/greeter.h
@@ -0,0 +1,53 @@
+/* -*- Mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#ifndef SAMPLE_GREETER_H
+#define SAMPLE_GREETER_H
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define SAMPLE_TYPE_GREETER \
+  (sample_greeter_get_type())
+
+#define SAMPLE_GREETER(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), SAMPLE_TYPE_GREETER, SampleGreeter))
+
+#define SAMPLE_GREETER_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass), SAMPLE_TYPE_GREETER, SampleGreeterClass))
+
+#define SAMPLE_IS_GREETER(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), SAMPLE_GREETER))
+
+#define SAMPLE_IS_GREETER_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass), SAMPLE_TYPE_GREETER))
+
+#define SAMPLE_GREETER_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS((obj), SAMPLE_TYPE_GREETER, SampleGreeter_Class))
+
+typedef struct _SampleGreeter      SampleGreeter;
+typedef struct _SampleGreeterClass SampleGreeterClass;
+
+/**
+ * SampleGreeter:
+ *
+ * A SampleGreetergreets.
+ */
+struct _SampleGreeter
+{
+  /*< private >*/
+  GObject parent_instance;
+};
+
+struct _SampleGreeterClass
+{
+  GObjectClass parent_class;
+};
+
+GType          sample_greeter_get_type (void) G_GNUC_CONST;
+SampleGreeter *saple_greeter_new       (void);
+const gchar   *sample_greeter_greet    (SampleGreeter *greeter);
+
+G_END_DECLS
+
+#endif /* SAMPLE_GREETER_H */
